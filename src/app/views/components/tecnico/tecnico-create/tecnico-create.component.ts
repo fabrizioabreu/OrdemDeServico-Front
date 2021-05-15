71a7741131025bedcb0fbb8ec6544d17feefcd15
Fabrizio Abreu <fabrizioabreu@live.com>
2021-05-14 21:47:06 -0300

    Tratamento de Exceções

--- src/app/views/components/tecnico/tecnico-create/tecnico-create.component.ts
+++ src/app/views/components/tecnico/tecnico-create/tecnico-create.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Tecnico } from 'src/app/models/tecnico';
+import { TecnicoService } from 'src/app/services/tecnico.service';
 
 @Component({
   selector: 'app-tecnico-create',
@@ -8,7 +10,15 @@ import { Router } from '@angular/router';
 })
 export class TecnicoCreateComponent implements OnInit {
 
+  tecnico: Tecnico = {
+    id: '',
+    nome: 'Rui Pinto',
+    cpf: '358.901.010-05',
+    telefone: '(00) 90011-8001'
+  }
+
   constructor(
+    private service : TecnicoService,
     private router : Router
   ) { }
 
@@ -17,6 +27,18 @@ export class TecnicoCreateComponent implements OnInit {
 
   cancel():void {
     this.router.navigate(['tecnicos'])
+  }
+
+  create():void {
+    this.service.create(this.tecnico).subscribe((resposta) => {
+      this.router.navigate(['tecnicos'])
+      this.service.message('Tecnico Criado com Sucesso!')
+    }, err => {
+      console.log(err)
+      if(err.error.error.match('já cadastrado')){
+        this.service.message(err.error.error)
+      }
+    })
   }
 
 }
